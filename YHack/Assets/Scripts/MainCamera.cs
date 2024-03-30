594be25e1930059d0c11f3e1f735baba3b7e21b6
NikitaSaitov <nik.saitov.04@gmail.com>
2024-03-30 16:02:32 -0400

    added main menu scene and TMP UI package

---
+++ YHack/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainCamera : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {   
+        SceneManager.LoadSceneAsync(1);
+    }
+}
+
