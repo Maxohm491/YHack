0f715af250e83ea61f5c52534cbfeb35093977cc
Max Ohm <max.ohm@yale.edu>
2024-03-30 12:22:49 -0400

    State checking and more movement

--- YHack/Assets/Scripts/PlayerMovement.cs
+++ YHack/Assets/Scripts/PlayerMovement.cs
@@ -11,24 +11,71 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField]
-    float speed = 5f;
+    private float speed = 5f;
+
+    [SerializeField]
+    private float gravStrength = 0.5f;
+
+    [SerializeField]
+    private float rocketPower = 1f;
+
+    private float currUpSpeed = 0f;
+
+    private State state;
+
+    enum State {
+        Grounded,
+        Floating
+    }
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        state = State.Floating;
     }
     
     void FixedUpdate()
     {
+        switch(state) {
+            case State.Floating:
+                FloatMovement();
+                break;
+        }
+    }
+
+    void FloatMovement() {
         float vert = Input.GetAxisRaw("Vertical");
         float hori = Input.GetAxisRaw("Horizontal");
 
         Vector2 planetToShip = transform.position - planet.transform.position;
-        Vector2 newVector = Quaternion.AngleAxis(-hori * speed, Vector3.forward) * planetToShip;
+        Vector2 rotatedPos = Quaternion.AngleAxis(-hori * speed, Vector3.forward) * planetToShip;
 
-        Vector2 grav = -planetToShip;
+        currUpSpeed -= gravStrength;
+        currUpSpeed += rocketPower * vert;
 
-        rb.MovePosition(newVector);
+        Vector2 upVector = rotatedPos.normalized * currUpSpeed;
+
+        Vector2 newPos = rotatedPos + upVector;
+        rb.MovePosition(newPos);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {   
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            state = State.Grounded;
+            Debug.Log("grounded");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {   
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            state = State.Floating;
+            Debug.Log("floating");
+        }
+
     }
 }
