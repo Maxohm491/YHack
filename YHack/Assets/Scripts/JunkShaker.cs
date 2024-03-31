ecfba9417fc82da663e54084c4327e2ec9a0df94
Max Ohm <max.ohm@yale.edu>
2024-03-30 20:52:22 -0400

    Changing junk

--- YHack/Assets/Scripts/JunkShaker.cs
+++ YHack/Assets/Scripts/JunkShaker.cs
@@ -21,6 +21,7 @@ public class JunkShaker : MonoBehaviour
 
     void Start() {
         target = Quaternion.AngleAxis(Random.value * 360 / maxRadius, Vector3.forward) * (Vector2.up * maxRadius);
+        transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
     }
 
     // Update is called once per frame
